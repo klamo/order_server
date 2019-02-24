ab2abfa1b497adcc97291193459d684d07e003e8
klamo <whuachi@163.com>
2019-02-24 16:58:25 +0800

    Feign结合Hystrix实现熔断

--- src/main/java/net/klamo/order_server/controller/OrderController.java
+++ src/main/java/net/klamo/order_server/controller/OrderController.java
@@ -1,12 +1,16 @@
 package net.klamo.order_server.controller;
 
 
+import com.google.common.collect.Maps;
 import com.netflix.discovery.converters.Auto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import net.klamo.order_server.server.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -21,8 +25,18 @@ public class OrderController {
     }
 
     @RequestMapping("findById")
+    @HystrixCommand(fallbackMethod = "findByIdDefaultStores")
     public Object findById(@RequestParam("id") int id){
-        return productOrderService.findById(id);
+        HashMap<String, Object> data = Maps.newHashMap();
+        data.put("code",-1);
+        data.put("data",productOrderService.findById(id));
+        return data;
     }
 
+    private Object findByIdDefaultStores(int id){
+        HashMap<String, Object> msg = Maps.newHashMap();
+        msg.put("code",0);
+        msg.put("data","调用人数太多，请稍后再试");
+        return msg;
+    }
 }
